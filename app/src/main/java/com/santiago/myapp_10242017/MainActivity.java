67bfd66f40976d9e31bdb09b5397d723ed986a56
aquilinojoshiahrowensantiago <2014074067@ust-iics.mygbiz.com>
2017-10-24 22:48:35 +0800

    Initial commit

---
+++ app/src/main/java/com/santiago/myapp_10242017/MainActivity.java
@@ -0,0 +1,46 @@
+package com.santiago.myapp_10242017;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnSave, btnDisplay;
+    TextView tvDisplay;
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsername = (EditText) findViewById(R.id.et_Username);
+        etPassword = (EditText) findViewById(R.id.et_Password);
+        btnSave = (Button) findViewById(R.id.btn_save);
+        btnDisplay = (Button) findViewById(R.id.btn_display);
+        preferences = getPreferences(Context.MODE_PRIVATE);
+
+    }
+    public void saveInfo(View view){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("username",etUsername.getText().toString());
+        editor.putString("password",etPassword.getText().toString());
+        editor.apply();
+        Toast.makeText(this, "Date Saved!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void loadInfo(View view){
+        String user = preferences.getString("username", "");
+        String pwd = preferences.getString("password", "");
+        tvDisplay.setText("The password of " + user + " is " + pwd);
+
+    }
+}
